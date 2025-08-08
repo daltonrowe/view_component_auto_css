6442a9555b88cda4859c757543973766d692801b
Dalton <cdaltonrowe@gmail.com>
2025-08-08 12:43:29 +0200

    test import demo

--- test/system/demo_test.rb
+++ test/system/demo_test.rb
@@ -13,4 +13,14 @@ class DemoTest < ApplicationSystemTestCase
     assert_backgrounds ".demo2_component", 1, "yellow"
     assert_backgrounds ".demo3_component", 1, "blue"
   end
+
+  test "visiting the import demo" do
+    visit import_demo_url
+
+    assert_inlined_style "demo_import_component"
+    assert_linked_style "demo_import2_component"
+
+    assert_backgrounds ".demo_import_component", 1, "purple"
+    assert_backgrounds ".demo_import2_component", 1, "yellow"
+  end
 end
